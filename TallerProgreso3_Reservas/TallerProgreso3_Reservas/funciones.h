a4a645905e631d45757aeda86f61a3f005e1592d
carlosllumi <168133573+carlosllumi@users.noreply.github.com>
2024-07-04 15:07:29 -0500

    Add files via upload

---
+++ TallerProgreso3_Reservas/TallerProgreso3_Reservas/funciones.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void buscarPorTamano(int *numHabitacion, char habitaciones[][3][40], double precios[]);
+void buscarHotel(int *numHabitacion, char habitaciones[][3][40], double precios[]);
+void realizarReserva(int numHabitacion, char habitaciones[][3][40], char clientes[][2][40], int reservas[][4]);
+void buscarReservaPorCedula(int *numReserva, char clientes[][2][40], int reservas[][4]);
+void imprimirReserva(int numReserva, int reservas[][4], char habitaciones[][3][40], double precios[]);
+void pagarReserva(int numReserva, int reservas[][4], char habitaciones[][3][40], double precios[]);
+
+// Nuevas funciones
+void cargarClientes(char clientes[][2][40]);
+void guardarClientes(char clientes[][2][40]);
+void cargarReservas(int reservas[][4]);
+void guardarReservas(int reservas[][4]);
+
